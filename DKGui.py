81b0d8482afff44c11e713ee2b077e7d2826f9c4
phuc <thanhnguyen180416@gmail.com>
2020-08-28 10:10:36 +0700

    update

--- DKGui.py
+++ DKGui.py
@@ -9,8 +9,13 @@ def hoc():
     if i == 1:
         radio_test = IntVar()
     print('Da Nhan vao button hoc')
-    Radiobutton(group1, text='Vi tri:' + str(i), variable=radio_test, value=i).grid(row=i+1, column=0, sticky=W+N)
+    Radiobutton(group1, text='Vi tri:' + str(i), variable=radio_test, value=i, command=pr).grid(row=i+1, column=0, sticky=W+N)
+    
     i=i+1
+def pr():
+    vt = radio_test.get()
+    print('Vi tri duoc chon:'+str(vt) )
+
 def dichuyen():
     vt = radio_test.get()
     print('di chuyen den: '+ str(vt))
@@ -28,5 +33,7 @@ btn_test = Button(group1,text="Di Chuyen", command=dichuyen, height=2, width=10)
 btn_test.grid(row=1, column=2, columnspan=2)
 
 
+lbl_test = Label(group1)
+lbl_test.grid(row = 1, column = 3 , sticky=W)
 
 root.mainloop()
