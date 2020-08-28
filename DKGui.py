7554789893d19cb4962d809472b1e583767c2e8d
phuc <thanhnguyen180416@gmail.com>
2020-08-28 10:50:49 +0700

    up

--- DKGui.py
+++ DKGui.py
@@ -6,8 +6,7 @@ i = 1
 def hoc():
     global i
     global radio_test
-    if i == 1:
-        radio_test = IntVar()
+   
     print('Da Nhan vao button hoc')
     Radiobutton(group1, text='Vi tri:' + str(i), variable=radio_test, value=i, command=pr).grid(row=i+1, column=0, sticky=W+N)
     i=i+1
@@ -25,7 +24,8 @@ def dichuyen():
 root = Tk()
 # Group1 Frame ----------------------------------------------------
 
-group1 = LabelFrame(root, text="Di Chuyen Robot", padx=50, pady=50)
+radio_test = IntVar()
+group1 = LabelFrame(root, text="Arar", padx=50, pady=50)
 group1.grid(row=0, column=0, padx=10, pady=10, sticky=E+W+N+S)
 
 btn_test = Button(group1,text="Hoc", command=hoc, height=2, width=10)
@@ -33,8 +33,19 @@ btn_test.grid(row=1, column=0, columnspan=2)
 btn_test = Button(group1,text="Di Chuyen", command=dichuyen, height=2, width=10)
 btn_test.grid(row=1, column=2, columnspan=2)
 
-
 lbl_test = Label(group1, text = "Vi Tri Duoc Chon:")
 lbl_test.grid(row = 0, column = 0, sticky=W)
+
+
+group2 = LabelFrame(root, text="Dieu Khien", padx=50, pady=50)
+group2.grid(row=0, column=1, padx=10, pady=10, sticky=E+W+N)
+btn_len = Button(group2,text="Len", command='', height=2, width=5)
+btn_len.grid(row=1, column=2, columnspan=2)
+btn_trai = Button(group2,text="Trai", command='', height=2, width=5)
+btn_trai.grid(row=2, column=0, columnspan=2)
+btn_xuong = Button(group2,text="Xuong", command='', height=2, width=5)
+btn_xuong.grid(row=2, column=2, columnspan=2)
+btn_phai = Button(group2,text="Phai", command='', height=2, width=5)
+btn_phai.grid(row=2, column=4, columnspan=2)
 
 root.mainloop()
